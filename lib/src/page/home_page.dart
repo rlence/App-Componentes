25c678bb257abc1a98c62988c6cc16e42abd56d9
rlence <lencericardo@gmail.com>
2019-08-10 13:37:33 +0200

    agregando comentarios

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -14,18 +14,20 @@ class HomePage extends StatelessWidget {
     );
   }
 
-
+  //este en el constructor del future de esta manera recojemos los datos
   Widget _list(){
 
     return FutureBuilder(
-      future: menuProvaider.cargarData(),
+      future: menuProvaider.cargarData(),//indicamos de donde viene el future
       //initialData: Text('Loading...'),//informacion por defecto mientras no se resuelva el future
+      //con el builder pasamos el context y el asyncSnapshot de lista dinamica ya que es un array
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
         
         print('builder');
         print(snapshot.data);
 
         return ListView(
+          //retornamos al clase listView como hijo nuestro componenet a renderizar
           children: _listItems( snapshot.data ),
         );
 
@@ -35,21 +37,23 @@ class HomePage extends StatelessWidget {
     
   }
 
-
+  //este es el componente que utilizamos para renderizar todas las propiedades del json
   List<Widget>  _listItems( List<dynamic> data ) {
 
+    //creamos una varianles que sera un list Widget que sea lo que retornemos
     final List<Widget> opciones = [];
 
+    //hacemos un forEach o .map para recorrer el arreglo o List
     data.forEach( (opt){
 
-      final widgetTemporal = ListTile(
-        title: Text(opt['texto']),
+      final widgetTemporal = ListTile( //creamos una variable temporal que es igual a una list
+        title: Text(opt['texto']),//pasamos la variable que los recorre y indicamos el elemento que se va a imprimir
         leading: Icon(Icons.account_circle, color: Colors.blue),
         trailing: Icon(Icons.keyboard_arrow_right, color:Colors.blue),
         onTap: (){}, 
       );
-      opciones..add(widgetTemporal)
-              ..add( Divider() );
+      opciones..add(widgetTemporal) //pasa,ps a las opciones el widget que creamos
+              ..add( Divider() ); //pasamos el divaider
     });
 
     return opciones;
