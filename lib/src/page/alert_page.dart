c36cfaa58963ad259a75ab0c1acd66d63b9c31f1
rlence <lencericardo@gmail.com>
2019-08-13 21:11:19 +0200

    programando nuestra propia alerta

--- lib/src/page/alert_page.dart
+++ lib/src/page/alert_page.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 
 class AlertPage extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Alert Page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostart alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: ()=> _mostrarAlert(context),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
@@ -16,4 +26,42 @@ class AlertPage extends StatelessWidget {
       ),
     );
   }
+
+  void _mostrarAlert( BuildContext context ){
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(25.0) ),
+          title: Text('titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, //con esto podemos reducir el tamaño de la columna al contenido o al maximo que puede dar
+            children: <Widget>[
+              Text('Este es el contenido de la caja de alerta'),
+              FlutterLogo(size: 100.0,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: ()=> Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+
+      }
+    );
+
+  }
+
+
 }
