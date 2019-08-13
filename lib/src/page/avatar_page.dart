5813b6aebc55f7bcce666bb858b9b25464eb791b
rlence <lencericardo@gmail.com>
2019-08-13 21:28:40 +0200

    creando componente de avatar

--- lib/src/page/avatar_page.dart
+++ lib/src/page/avatar_page.dart
@@ -7,12 +7,37 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1105031938224672768/hdiU1R3y.png'),
+              radius: 25.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('RL'),
+              backgroundColor: Colors.lightGreenAccent,
+          ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://pbs.twimg.com/profile_images/1105031938224672768/hdiU1R3y.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
