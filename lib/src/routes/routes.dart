4652fe5e3965d1b0daa9e917e9e6d4f72c7530eb
rlence <lencericardo@gmail.com>
2019-08-19 21:28:57 +0200

    creando el listview.builder

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -7,6 +7,7 @@ import 'package:componentes/src/page/avatar_page.dart';
 import 'package:componentes/src/page/home_page.dart';
 import 'package:componentes/src/page/inputs_page.dart';
 import 'package:componentes/src/page/slider_page.dart';
+import 'package:componentes/src/page/listview_page.dart';
 
 Map <String, WidgetBuilder> getAplicationRoutes(){
 
@@ -18,6 +19,9 @@ Map <String, WidgetBuilder> getAplicationRoutes(){
         'animetedContainer':(BuildContext context) => AnimatedContainerPage(),
         'inputs':(BuildContext context) => InputPage(),
         'sliders':(BuildContext context) => SliderPage(),
+        'list':(BuildContext context) => ListaPage(),
+        
+        
         
       };
 
