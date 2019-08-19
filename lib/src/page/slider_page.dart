87a3dc5b2fd54ba04478be93b87f7d94a1898bab
rlence <lencericardo@gmail.com>
2019-08-19 21:16:30 +0200

    creando un cehckBox y un switch

--- lib/src/page/slider_page.dart
+++ lib/src/page/slider_page.dart
@@ -9,6 +9,7 @@ class _SliderPageState extends State<SliderPage> {
 
 
   double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +22,8 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _crearSlider(),
+            _crearCheckBox(),
+            _crearSwitch(),
             Expanded(
               child: _crearImagen()
               ),
@@ -40,11 +43,10 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlider,
       min: 10.0,
       max: 400.0,
-      onChanged: ( valor ){
-          
+      onChanged: (_bloquearCheck) ? null //esto es para bloquear la barra si el checkbox esta activo
+        :( valor ){
           setState(() {
             _valorSlider = valor;
-
           });
       },
     );
@@ -60,6 +62,49 @@ class _SliderPageState extends State<SliderPage> {
       fit: BoxFit.contain,
     );
 
+
+  }
+
+  //metodo para crear un checkbox
+  Widget _crearCheckBox() {
+
+    //primera forma de crear el checkbox
+    // return Checkbox(
+    //   value: _bloquearCheck,
+    //   onChanged: ( valor ){
+    //     setState(() {
+    //       _bloquearCheck = valor;
+    //     });
+    //   },
+    // );
+
+    //segunda forma y mas recomendada para crear un checkbox con texto
+    return CheckboxListTile(
+      title: Text('Bloquear el Slider'),
+      value: _bloquearCheck,
+       onChanged: ( valor ){
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+
+    );
+
+  }
+
+  //creando un switch
+  Widget _crearSwitch() {
+
+    return SwitchListTile(
+      title: Text('Bloquear el Slider'),
+      value: _bloquearCheck,
+       onChanged: ( valor ){
+        setState(() {
+          _bloquearCheck = valor;
+        });
+      },
+
+    );
 
   }
 }
